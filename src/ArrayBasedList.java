31bec881e8f30a7d5f84b8f5d9719fe1fc25dca0
KelvinQiu <kelvinqiu802@outlook.com>
2022-11-22 21:30:46 +0800

    feat: array based list

--- src/ArrayBasedList.java
+++ src/ArrayBasedList.java
@@ -3,124 +3,131 @@ public class ArrayBasedList {
   private int size;
   private int maxSize;
 
-  public static void main(String[] args) {
-    ArrayBasedList arr = new ArrayBasedList();
-    arr.insertFirst(200);
-    arr.insertBefore(0, 1);
-    arr.insertBefore(0, 1);
-    arr.printArr();
-  }
-
   public ArrayBasedList() {
-    this.maxSize = 10;
     this.size = 0;
+    this.maxSize = 2;
     this.items = new ArrPos[maxSize];
   }
 
-  public void grow() {
-    ArrPos[] newArr = new ArrPos[maxSize * 2];
-    maxSize *= 2;
-    for (int i = 0; i < size; i++) {
-      newArr[i] = items[i];
+  public ArrPos insertFirst(int d) {
+    ArrPos n = new ArrPos(d, 0);
+    for (int i = size - 1; i >= 0; i--) {
+      items[i].increment();
+      items[i + 1] = items[i];
     }
-    items = newArr;
+    items[0] = n;
+    size++;
+    changeSize();
+    return n;
+  }
+
+  public ArrPos insertLast(int d) {
+    ArrPos n = new ArrPos(d, size);
+    items[size] = n;
+    size++;
+    changeSize();
+    return n;
+  }
+
+  public ArrPos insertAfter(ArrPos p, int d) {
+    ArrPos n = new ArrPos(d, p.getIndex() + 1);
+    for (int i = size - 1; i > p.getIndex(); i--) {
+      items[i].increment();
+      items[i + 1] = items[i];
+    }
+    items[n.getIndex()] = n;
+    size++;
+    changeSize();
+    return n;
+  }
+
+  public ArrPos insertBefore(ArrPos p, int d) {
+    ArrPos n = new ArrPos(d, p.getIndex());
+    for (int i = size - 1; i >= p.getIndex(); i--) {
+      items[i].increment();
+      items[i + 1] = items[i];
+    }
+    items[n.getIndex()] = n;
+    size++;
+    changeSize();
+    return n;
+  }
+
+  public void remove(ArrPos p) {
+    for (int i = p.getIndex() + 1; i < size; i++) {
+      items[i].decrement();
+      items[i - 1] = items[i];
+    }
+    size--;
+    changeSize();
+  }
+
+  public void grow() {
+    maxSize *= 2;
+    ArrPos[] arr = new ArrPos[maxSize];
+    for (int i = 0; i < size; i++) {
+      arr[i] = items[i];
+    }
+    items = arr;
   }
 
   public void shrink() {
-    ArrPos[] newArr = new ArrPos[maxSize / 2];
     maxSize /= 2;
+    ArrPos[] arr = new ArrPos[maxSize];
     for (int i = 0; i < size; i++) {
-      newArr[i] = items[i];
+      arr[i] = items[i];
     }
-    items = newArr;
+    items = arr;
   }
 
   public void changeSize() {
-    if (size >= maxSize) {
+    if (size + 1 >= maxSize) {
       grow();
-      return;
-    }
-    if (size / maxSize <= 0.25) {
+    } else if ((float) size / maxSize < 0.25) {
       shrink();
-      return;
     }
   }
 
-  public ArrPos first() {
-    return items[0];
+  public int first() {
+    return items[0].element();
   }
 
-  public ArrPos last() {
-    return items[size];
+  public int last() {
+    return items[size - 1].element();
   }
 
   public int size() {
     return size;
   }
 
-  public boolean isEmpty() {
+  public Boolean isEmpty() {
     return size == 0;
   }
 
-  public ArrPos after(int p) {
-    return items[p + 1];
+  public ArrPos after(ArrPos p) {
+    return items[p.getIndex() + 1];
   }
 
-  public ArrPos before(int p) {
-    return items[p - 1];
+  public ArrPos before(ArrPos p) {
+    return items[p.getIndex() - 1];
   }
 
-  public void insertFirst(int d) {
-    ArrPos pos = new ArrPos(d, 0);
-    for (int i = 0; i < size; i++) {
-      items[i].increment();
-      items[i + 1] = items[i];
-    }
-    items[0] = pos;
-    size++;
-  }
-
-  public void insertLast(int d) {
-    ArrPos pos = new ArrPos(d, size);
-    items[size] = pos;
-    size++;
-  }
-
-  public void insertAfter(int p, int d) {
-    // TODO FIX
-    ArrPos pos = new ArrPos(d, p + 1);
-    for (int i = p + 1; i < size; i++) {
-      items[i].increment();
-      items[i + 1] = items[i];
-    }
-    items[p + 1] = pos;
-    size++;
-  }
-
-  public void insertBefore(int p, int d) {
-    // TODO FIX
-    ArrPos pos = new ArrPos(d, p);
-    for (int i = p; i < size; i++) {
-      items[i].increment();
-      items[i + 1] = items[i];
-    }
-    items[p] = pos;
-    size++;
-  }
-
-  public ArrPos remove(int p) {
-    ArrPos pos = items[p];
-    for (int i = p + 1; i < size; i++) {
-      items[i].decrement();
-      items[i] = items[i + 1];
-    }
-    size--;
-    return pos;
-  }
-
-  public void printArr() {
+  public void print() {
     for (int i = 0; i < size; i++) {
       System.out.println(items[i].element());
     }
+  }
+
+  public static void main(String[] args) {
+    ArrayBasedList list = new ArrayBasedList();
+    list.insertFirst(1);
+    list.insertFirst(0);
+    list.insertLast(3);
+    ArrPos a = list.insertLast(4);
+    ArrPos b = list.insertAfter(a, 10);
+    ArrPos c = list.insertBefore(b, 11);
+    list.remove(c);
+    // 0, 1, 3, 4, 10
+    list.print();
   }
 }
